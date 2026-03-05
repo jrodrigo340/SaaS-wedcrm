4c1b82648555d8dfacc8148b883a103f660afb30
João Rodrigo <joaorodrigooliver9@gmail.com>
2026-03-04 22:42:37 -0300

    configuração inicial do projeto Spring Boot com Flyway e PostgreSQL

---
+++ src/main/java/com/wedcrm/WedcrmApplication.java
@@ -0,0 +1,13 @@
+package com.wedcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedcrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WedcrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wedcrm/WedcrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WedcrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
